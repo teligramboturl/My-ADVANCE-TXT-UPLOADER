1f5f5f21a467f3329b0c74cb6f752499f1039bfd
teligramboturl <teligramboturl@gmail.com>
2025-09-07 02:30:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "23673651")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8316655385:AAG2hFziKz-wlH1l4H4u9PtHBs_QkYh_Zak") #Replace with your bot token
